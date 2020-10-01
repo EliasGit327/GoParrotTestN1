7d891e215e953aef8e5177c708ebb07febeb2ff9
Ilie <ilie.chukitu@gmail.com>
2020-10-01 12:30:42 +0300

    - Added styled components.

--- App.tsx
+++ App.tsx
@@ -1,6 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
 
 const App = () => {
   return (
